9f853cafa6ae6927fe53a71ce7c2f597466b4932
Liwenju <onebigdream@qq.com>
2016-06-22 00:42:38 +0800

    添加新闻实体类

---
+++ app/src/main/java/com/ay7/as/New.java
@@ -0,0 +1,42 @@
+package com.ay7.as;
+
+/**
+ * Created by Administrator on 2016/6/22.
+ */
+public class New {
+
+
+    /**
+     * newStyle : life
+     * newEditor : juzi
+     * newNum : 33
+     */
+
+    private String newStyle;
+    private String newEditor;
+    private String newNum;
+
+    public String getNewStyle() {
+        return newStyle;
+    }
+
+    public void setNewStyle(String newStyle) {
+        this.newStyle = newStyle;
+    }
+
+    public String getNewEditor() {
+        return newEditor;
+    }
+
+    public void setNewEditor(String newEditor) {
+        this.newEditor = newEditor;
+    }
+
+    public String getNewNum() {
+        return newNum;
+    }
+
+    public void setNewNum(String newNum) {
+        this.newNum = newNum;
+    }
+}
