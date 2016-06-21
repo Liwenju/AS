bf69547bb4a236987a201768b8b7a30089b44319
Liwenju <onebigdream@qq.com>
2016-06-22 01:22:52 +0800

    增加插件之后的代码

--- app/src/main/java/com/ay7/as/MainActivity.java
+++ app/src/main/java/com/ay7/as/MainActivity.java
@@ -2,20 +2,31 @@ package com.ay7.as;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private int oldNum=0;
+    @Bind(R.id.tv)
+    private
+    TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
 
     }
 
 
-
-
+    @OnClick(R.id.tv)
+    public void onClick() {
+        tv.setVisibility(View.GONE);
+    }
 }
